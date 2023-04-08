3d91d27e76b9e75098a16b5fc4885ebd6ede8918
Ehtimam <70962870+Ehtimam@users.noreply.github.com>
2023-04-08 16:04:02 +0600

    ADD: 0.7

--- myapp/models/companyModels.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Company(models.Model):
-    company_name = models.CharField(max_length=100)
-    company_address = models.CharField(max_length=200)
-
-    class Meta:
-        app_label = 'myapp'
-        db_table = 'company'
