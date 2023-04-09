176061396e8367423fa49a533242998b8091c178
Ehtimam <70962870+Ehtimam@users.noreply.github.com>
2023-04-10 03:09:16 +0600

    ADD: changed everything

--- myapp/controllers/Company.py
+++ myapp/controllers/Company.py
@@ -1,42 +1,68 @@
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import status, viewsets
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from myapp.modelss.Company import Company
-from myapp.serializers.serializer import CompanySerializer
 
+@csrf_exempt
+# This is add company funtion
+def add_companies(request):
+    #I am using postman to send a get request with json componenets
+    if request:
+        data = request.POST
+        #json components are below, The company_id is auto incremented
+        company_name = data.get('company_name')
+        company_address = data.get('company_address')
+        
+        #if company name already exists in the database it will prevent in regestaring comapny name
+        
+        
+        company = Company(company_name=company_name, company_address=company_address)
+        company.save()
+        return JsonResponse({'status': 'success',
+                             'company_id':company.id}, status = 201)
+    else:
+        return JsonResponse({'status': 'error'}, status = 400)
 
-class CompanyViewSet(viewsets.ModelViewSet):
-    queryset = Company.objects.all()
-    serializer_class = CompanySerializer
-    @api_view(['POST'])
-    def add_companies(request):
-            serializer = CompanySerializer(data=request.data)
-            if serializer.is_valid():
-                company = serializer.save()
-                return Response({'status': 'success', 'company_id': company.id}, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    @api_view(['PUT'])
-    def update_companies(request, id):
+#This is for updating the company information such as name and address
+def update_companies(request, id):
+    if request:
+        data = request.POST
+        company_id = id
+        company_name = data.get('company_name')
+        company_address = data.get('company_address')
+        
         try:
-            company = Company.objects.get(id=id)
-            
+            company = Company.objects.get(id = company_id)
         except Company.DoesNotExist:
-            return Response({'status': 'error', 'message': 'Company does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse({'status':'error', 'message':'Company does not exists'}, status = 401)
+        company.company_name = company_name
+        company.company_address = company_address
+        company.save()
+        return JsonResponse({'status':'success'}, status = 201)
+    else:
+        return JsonResponse({'status':'error'}, status = 400)
 
-        serializer = CompanySerializer(company, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'status': 'success'}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#This is for pulling the company information from the database through the comapny id
+def get_companies(request, id):
+    if request:
+        company_id = id
+        if company_id:
+            try:    
+                
+                company = Company.objects.get(id = company_id)
+                return JsonResponse({
+                    'company_name':company.company_name,
+                    'company_address': company.company_address,
+                    'company_id': company.id
+                })
+            except:
+                return JsonResponse({
+                    'status':'error',
+                    'message':'Company information cannot be retrive due to company id and the request id is not matching.'
+                },status = 401)
+        else:
+            return JsonResponse({'status':'error',
+                                 'message':'Missing Company_id'},status = 404)
+    else:
+        return JsonResponse({'status':'error'},status = 400)
 
-    @api_view(['GET'])
-    def get_companies(request, id):
-        try:
-            company = Company.objects.get(id=id)
-        except Company.DoesNotExist:
-            return Response({'status': 'error', 'message': 'Company does not exist.'}, status=status.HTTP_404_NOT_FOUND)
 
-        serializer = CompanySerializer(company)
-        return Response(serializer.data, status=status.HTTP_200_OK)
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,77 +1,77 @@
-# from django.urls import path, include
-# from . import views
-# from myapp.controllers.Company import add_companies, update_companies, get_companies
-# from myapp.controllers.Employee import add_employees,show_employees, update_employees
-# urlpatterns = [
-    
-#     # path('add_person/', views.add_person, name='add_person'),
-#     # add any other URL patterns for your app here
-#     path('api/companies/', add_companies, name='add_companies'),
-#     path('api/companies/<int:id>/', update_companies, name='update_companies'),
-#     path('api/companies/<int:id>/', get_companies, name='get_companies'),
-    
-
-#     path('api/employees/<int:id>/', add_employees, name='add_employees'),
-#      path('api/employees/update/<int:id>/<int:emp_id>/', update_employees, name='update_employees'),
-#     path('api/employees/show/<int:id>/', show_employees, name='show_employees'),
-
-# ]
-
 from django.urls import path, include
-from rest_framework import routers
-from myapp.controllers.Company import CompanyViewSet
-from myapp.controllers.Employee import EmployeeViewSet
+from . import views
+from myapp.controllers.Company import add_companies, update_companies, get_companies
+from myapp.controllers.Employee import add_employees,show_employees, update_employees
+urlpatterns = [
+    
+    # path('add_person/', views.add_person, name='add_person'),
+    # add any other URL patterns for your app here
+    path('api/companies/', add_companies, name='add_companies'),
+    path('api/companies/<int:id>/', update_companies, name='update_companies'),
+    path('api/companies/<int:id>/', get_companies, name='get_companies'),
+    
+
+    path('api/employees/<int:id>/', add_employees, name='add_employees'),
+     path('api/employees/update/<int:id>/<int:emp_id>/', update_employees, name='update_employees'),
+    path('api/employees/show/<int:id>/', show_employees, name='show_employees'),
+
+]
+
+# from django.urls import path, include
+# from rest_framework import routers
+# from myapp.controllers.Company import CompanyViewSet
+# from myapp.controllers.Employee import EmployeeViewSet
 
 
+# # router = routers.SimpleRouter()
+# # router.register(r'companies', CompanyViewSet, basename='CompanyViewSet')
+
+# # # Define a nested router for employees under the companies router
+# # companies_router = routers.DefaultRouter()
+# # companies_router.register(r'employees', EmployeeViewSet, basename='EmployeeViewSet')
+
+# # urlpatterns = [
+# #     path('api/', include(router.urls)),
+# #     path('api/companies/', include(companies_router.urls)),
+# #     #  path('api/companies/<int:company_id>/employees/', EmployeeViewSet.as_view({'get': 'app_employees'}), name='app_employees'),
+# # ]
 # router = routers.SimpleRouter()
 # router.register(r'companies', CompanyViewSet, basename='CompanyViewSet')
-
-# # Define a nested router for employees under the companies router
-# companies_router = routers.DefaultRouter()
-# companies_router.register(r'employees', EmployeeViewSet, basename='EmployeeViewSet')
+# router.register(r'employees', EmployeeViewSet, basename='EmployeeViewSet')
 
 # urlpatterns = [
 #     path('api/', include(router.urls)),
-#     path('api/companies/', include(companies_router.urls)),
-#     #  path('api/companies/<int:company_id>/employees/', EmployeeViewSet.as_view({'get': 'app_employees'}), name='app_employees'),
+#     path('api/', include(router.urls)),
 # ]
-router = routers.SimpleRouter()
-router.register(r'companies', CompanyViewSet, basename='CompanyViewSet')
-router.register(r'employees', EmployeeViewSet, basename='EmployeeViewSet')
-
-urlpatterns = [
-    path('api/', include(router.urls)),
-    path('api/', include(router.urls)),
-]
 
 
 
 
-# company_list = CompanyViewSet.as_view({
-#     'get': 'list',
-#     'post': 'create'
-# })
-# company_detail = CompanyViewSet.as_view({
-#     'get':'get_companies', 
-#     'put':'update_compaines'
-# })
+# # company_list = CompanyViewSet.as_view({
+# #     'get': 'list',
+# #     'post': 'create'
+# # })
+# # company_detail = CompanyViewSet.as_view({
+# #     'get':'get_companies', 
+# #     'put':'update_compaines'
+# # })
 
-# employee_detail = EmployeeViewSet.as_view({
-#     'get': 'retrieve',
-#     'put': 'update',
-#     'delete': 'destroy'
-# })
+# # employee_detail = EmployeeViewSet.as_view({
+# #     'get': 'retrieve',
+# #     'put': 'update',
+# #     'delete': 'destroy'
+# # })
 
-# urlpatterns = [
-#     path('api/companies/', CompanyViewSet.as_view({'post':'add_companies'}), name='add-companies'),
-#     path('api/companies/<int:pk>/', company_detail, name='company-detail'),
-#     path('api/companies/<int:id>/employees/', EmployeeViewSet.as_view({'post': 'add_employees'}), name='add-employee'),
+# # urlpatterns = [
+# #     path('api/companies/', CompanyViewSet.as_view({'post':'add_companies'}), name='add-companies'),
+# #     path('api/companies/<int:pk>/', company_detail, name='company-detail'),
+# #     path('api/companies/<int:id>/employees/', EmployeeViewSet.as_view({'post': 'add_employees'}), name='add-employee'),
     
-# ]
+# # ]
 
-# urlpatterns = [
-#     path('companies/', company_list, name='company-list'),
-#     path('companies/<int:pk>/', company_detail, name='company-detail'),
-#     path('companies/<int:pk>/add_employee/', EmployeeViewSet.as_view({'post': 'add_employee'}), name='add-employee'),
-#     path('employees/<int:pk>/', employee_detail, name='employee-detail'),
-# ]
+# # urlpatterns = [
+# #     path('companies/', company_list, name='company-list'),
+# #     path('companies/<int:pk>/', company_detail, name='company-detail'),
+# #     path('companies/<int:pk>/add_employee/', EmployeeViewSet.as_view({'post': 'add_employee'}), name='add-employee'),
+# #     path('employees/<int:pk>/', employee_detail, name='employee-detail'),
+# # ]
